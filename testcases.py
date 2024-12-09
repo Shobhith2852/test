c0bbbda198b21f9e694f94c666ca04323bbfe77b
Shobhith2852 <144722453+Shobhith2852@users.noreply.github.com>
2024-12-09 16:46:25 -0700

    Update testcases.py

--- testcases.py
+++ testcases.py
@@ -9,51 +9,51 @@ class TestLibraryManagement(unittest.TestCase):
         self.book1 = Book("Title1", "Author1", "ISBN1", 2020, 15.99)
         self.book2 = Book("Title2", "Author2", "ISBN2", 2021, 10.99)
         self.book3 = Book("Another Title", "Author1", "ISBN3", 2019, 20.99)
-        self.library.add_book(self.book1)
-        self.library.add_book(self.book2)
-        self.library.add_book(self.book3)
+        self.library.add_book(self.book1)  # Corrected method name
+        self.library.add_book(self.book2)  # Corrected method name
+        self.library.add_book(self.book3)  # Corrected method name
 
     def test_add_book(self):
         self.assertEqual(len(self.library.books), 3)
         new_book = Book("New Title", "New Author", "ISBN4", 2022, 5.99)
-        self.library.add_book(new_book)
+        self.library.add_book(new_book)  # Corrected method name
         self.assertEqual(len(self.library.books), 4)
 
     def test_remove_book(self):
-        self.library.remove_book("ISBN2")
+        self.library.remove_book("ISBN2")  # Corrected method name
         self.assertEqual(len(self.library.books), 2)
-        self.assertIsNone(self.library.display_book_by_isbn("ISBN2"))
+        self.assertIsNone(self.library.display_book_by_isbn("ISBN2"))  # Corrected method name
 
     def test_search_by_title(self):
-        results = self.library.searchByTitle("Title")
+        results = self.library.search_by_title("Title")  # Corrected method name
         self.assertEqual(len(results), 2)
         self.assertIn(self.book1, results)
         self.assertIn(self.book2, results)
 
     def test_search_by_author(self):
-        results = self.library.searchByAuthor("Author1")
+        results = self.library.search_by_author("Author1")  # Corrected method name
         self.assertEqual(len(results), 2)
         self.assertIn(self.book1, results)
         self.assertIn(self.book3, results)
 
     def test_sort_by_price(self):
-        self.library.sortByPrice()
+        self.library.sort_by_price()  # Corrected method name
         self.assertEqual(self.library.books[0], self.book2)
         self.assertEqual(self.library.books[2], self.book3)
 
     def test_sort_by_year(self):
-        self.library.sortByYear()
+        self.library.sort_by_year()  # Corrected method name
         self.assertEqual(self.library.books[0], self.book3)
         self.assertEqual(self.library.books[2], self.book2)
 
     def test_display_book_by_isbn(self):
-        book = self.library.displayBookByISBN("ISBN1")
+        book = self.library.display_book_by_isbn("ISBN1")  # Corrected method name
         self.assertIsNotNone(book)
         self.assertEqual(book.title, "Title1")
 
     def test_update_book_details(self):
-        self.library.updateBookDetails("ISBN1")
-        updated_book = self.library.displayBookByISBN("ISBN1")
+        self.library.update_book_details("ISBN1")  # Corrected method name
+        updated_book = self.library.display_book_by_isbn("ISBN1")  # Corrected method name
         self.assertIsNotNone(updated_book)
         self.assertEqual(updated_book.title, "Updated Title")
 
@@ -66,12 +66,12 @@ class TestLibraryManagement(unittest.TestCase):
             file.write("Another Title\nAuthor1\nISBN3\n2019\n20.99\n")
         
         new_library = Library()
-        new_library.loadBooksFromFile(filename)
+        new_library.load_books_from_file(filename)  # Corrected method name
         self.assertEqual(len(new_library.books), 3)
         os.remove(filename)
 
     def test_clear_books(self):
-        self.library.clearBooks()
+        self.library.clear_books()  # Corrected method name
         self.assertEqual(len(self.library.books), 0)
 
     def test_display_total_books(self):
